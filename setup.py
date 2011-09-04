13c8b8bc6f3ca3132835ba7c3c36064be821a414
David Burke <david@burkesoftware.com>
2011-09-04 14:26:23 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-mass-edit",
+    version = "1.0",
+    author = "David Burke",
+    author_email = "david@burkesoftware.com",
+    description = ("Make bulk changes in the Django admin interface"),
+    license = "BSD",
+    keywords = "django admin",
+    url = "http://code.google.com/p/django-mass-edit/",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        "License :: OSI Approved :: BSD License",
+    ],
+)
