6eac412fcd402c368554d921ed8739e511f73c84
Naman Shenoy <namanshenoy@users.noreply.github.com>
2017-03-30 16:41:31 -0400

    Add Django Mass Edit Action to admin actions
    Django needed Mass Edit action to be added.

--- mass_demo/urls.py
+++ mass_demo/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from massadmin import urls as massadmin_urls
+from massadmin import mass_change_selected
+
+admin.site.add_action(mass_change_selected)
 admin.autodiscover()
 
 urlpatterns = [
