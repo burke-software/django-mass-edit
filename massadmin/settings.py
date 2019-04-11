884a21a323dd79fe66560fa8d80daa5e5c9fe6cf
Fran Hrzenjak <fran.hrzenjak@gmail.com>
2019-04-11 14:44:15 +0200

    settings for treshold for session based urls

--- massadmin/settings.py
+++ massadmin/settings.py
@@ -3,9 +3,14 @@ from django.conf import settings
 
 _default_settings = {
     'ADD_ACTION_GLOBALLY': True,
+    'SESSION_BASED_URL_THRESHOLD': 500,
 }
 
 _settings = getattr(settings, 'MASSEDIT', _default_settings)
 
 ADD_ACTION_GLOBALLY = _settings.get(
     'ADD_ACTION_GLOBALLY', _default_settings['ADD_ACTION_GLOBALLY'])
+
+
+SESSION_BASED_URL_THRESHOLD = _settings.get(
+    'SESSION_BASED_URL_THRESHOLD', _default_settings['SESSION_BASED_URL_THRESHOLD'])
