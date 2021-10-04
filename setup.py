978a42763e1513488b7fb5798739987ed942ad5f
Petr Dlouhý <petr.dlouhy@email.cz>
2021-10-04 13:59:25 +0200

    Bumped version for normal release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-mass-edit",
-    version = "3.2.0",
+    version="3.3.0",
     author = "David Burke",
     author_email = "david@burkesoftware.com",
     description = ("Make bulk changes in the Django admin interface"),
